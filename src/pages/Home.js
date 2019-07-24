a41611b631beef73d895af34a41e8826064f48b6
r.sabzi <r.kh.sabzi@gmail.com>
2019-07-24 18:47:06 +0430

    add Hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Hero from '../components/Hero'
+
 
 export default function Home() {
   return (
-    <div>
-      home
-    </div>
+    <Hero >
+
+    </Hero>
   )
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Hero from '../components/Hero'
+
 
 export default function Rooms() {
   return (
-    <div>
-      rooms
-    </div>
+    <Hero hero="roomsHero">
+
+    </Hero>
   )
 }
