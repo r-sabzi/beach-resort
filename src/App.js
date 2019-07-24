1d20ddd0e48d6b28641c9289f8a0e7a7efd87120
r.sabzi <r.kh.sabzi@gmail.com>
2019-07-24 18:20:15 +0430

    add Navbar

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
-
+import Navbar from './components/Navbar'
 function App() {
   return (
+    <>
+    <Navbar/>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/rooms" exact component={Rooms} />
       <Route path="/rooms/:slug" exact component={SingleRoom} />
       <Route component={Error} />
-    </Switch>
+      </Switch>
+      </>
   );
 }
 
