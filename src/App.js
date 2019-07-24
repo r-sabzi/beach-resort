e24c2d4fa52c3c843f6056ad272f5c0857b7faa6
r.sabzi <r.kh.sabzi@gmail.com>
2019-07-24 11:29:59 +0430

    setting up router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
 
 function App() {
   return (
-    <>
-      hello
-      </>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms" exact component={Rooms} />
+      <Route path="/rooms/:slug" exact component={SingleRoom} />
+      <Route component={Error} />
+    </Switch>
   );
 }
 
