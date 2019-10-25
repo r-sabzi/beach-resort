71bc8b6b391f8d7bc284d899fdb5c859484a4b09
r.sabzi <r.kh.sabzi@gmail.com>
2019-10-25 13:02:21 +0330

    adding featuredRooms, context data loading

--- src/context.js
+++ src/context.js
@@ -1,17 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import items from "./data";
 
-const RoomContext = React.createContext(); 
-
+const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
-  state = {};
+  state = {
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    let featuredRooms = rooms.filter(room => room.featured === true);
+    this.setState({
+      rooms, featuredRooms, sortedRooms: rooms, loading: false
+    });
+  }
+  
+  formatData(items) {
+    let tempItems = items.map(item => {
+      let id = item.sys.id;
+      let images = item.fields.images.map(image => image.fields.file.url);
+      let room = { ...item, images, id };
+      return room;
+    })
+    return tempItems;
+  }
 
   render() {
-    return (<RoomContext.Provider value={'dummy'}>
-      {this.props.children}
-    </RoomContext.Provider> )
+    return (
+      <RoomContext.Provider value={{ ...this.state }}>
+        {this.props.children}
+      </RoomContext.Provider>
+    );
   }
 }
 
 const RoomConsumer = RoomContext.Consumer;
-export {RoomProvider,RoomConsumer,RoomContext}
+export { RoomProvider, RoomConsumer, RoomContext };
