bedd05a3aa548b9f4daa6c55ea423e0369ec5b92
r.sabzi <r.kh.sabzi@gmail.com>
2019-10-25 12:16:44 +0330

    adding context

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import {RoomProvider} from './context'
 // import './index.css';
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>,
   document.getElementById("root")
 );
 
